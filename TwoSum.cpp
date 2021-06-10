1efbed08e13bb0b9c9e9d90db4a5a50a8dd87252
Hiếu Lã <hrcp.hieu@gmail.com>
2021-06-10 11:18:00 +0700

    [ADD] Solution TwoSum

---
+++ TwoSum.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <map>
+#include <iterator>
+#include <vector>
+
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+        map<int, int> m;
+        vector<int> res;
+
+        for(int i = 0; i < nums.size(); i++){
+            if(m.find(target - nums[i]) != m.end()){
+                res.push_back(m.find(target - nums[i])->second);
+                res.push_back(i);
+                return res;
+            }
+            else{
+                m[nums[i]] = i;
+            }
+        }
+        return res;
+}
+
+int main(){ 
+    /*
+        Input: nums = [2,7,11,15], target = 9
+        Output: [0,1]
+        Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+    */
+
+    vector <int> inp = {2,7,11,5}; 
+    int target = 9;
+
+    twoSum(inp, target);
+
+    return 0;
+}
